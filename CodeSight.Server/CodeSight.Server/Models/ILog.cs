9728307b0e594df0480169081676a4e8fdda2005
Amit Philips <a.p360@yahoo.com>
2015-11-30 23:50:36 +0530

    Added a ILog interface

---
+++ CodeSight.Server/CodeSight.Server/Models/ILog.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeSight.Server.Models
+{
+    interface ILog
+    {
+        string ProjectId { get; set; }
+        string MethodName { get; set; }
+        string Message { get; set; }
+        string LogType { get; set; }
+        List<string> Parameters { get; set; }
+        DateTime DateCreated { get; set; }
+
+        void WriteLog(ILog log);
+    }
+}
