26399c9b021496b7e6637164c6b771985298cadc
Amit Philips <a.p360@yahoo.com>
2015-12-03 11:19:23 +0530

    Log abstract class created

---
+++ CodeSight.Server/CodeSight.Server/Models/Log.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeSight.Server.Models
+{
+    public abstract class Log
+    {
+        protected string ProjectId { get; set; }
+        protected string MethodName { get; set; }
+        protected string Message { get; set; }
+        protected abstract string LogType { set; }
+        protected List<string> Parameters { get; set; }
+        protected DateTime DateCreated { set { value = DateTime.Now; } }
+
+        public abstract void WriteLog();
+    }
+}
--- CodeSight.Server/CodeSight.Server/Models/MessageLog.cs
+++ CodeSight.Server/CodeSight.Server/Models/MessageLog.cs
@@ -6,16 +6,15 @@ using System.Threading.Tasks;
 
 namespace CodeSight.Server.Models
 {
-    public class MessageLog : ILog
+    public class MessageLog : Log
     {
-        public string ProjectId { get; set; }
-        public string MethodName { get; set; }
-        public string LogType { set { LogType = "M"; } }
-        public string Message { get; set; }
-        public List<string> Parameters { get; set; }
-        public DateTime DateCreated { set { DateCreated = DateTime.UtcNow; } }
+        protected override string LogType { set { value = "M"; } }
+        public MessageLog()
+        {
 
-        public void WriteLog()
+        }
+
+        public override void WriteLog()
         {
 
         }
